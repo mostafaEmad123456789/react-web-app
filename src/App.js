e9d9a8344eaa64907c7877a80d58a5409a7260c2
mostafaEmad123456789 <mostafa5mes@gmail.com>
2021-08-31 14:22:07 +0200

    update AWS config

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Mostafa Emad Abdo
+          Learn React By Mostafa Emad
         </a>
       </header>
     </div>
